312cbe345b59ef180536ce13b8a8f74b0bdcb73b
Coos Baakman <cbaakman@radboudumc.nl>
2024-10-14 10:52:56 +0200

    make RelativePositionEncoder like SequenceEncoder in swiftmhc

--- seqwise/model.py
+++ seqwise/model.py
@@ -1,136 +1,167 @@
 from typing import Optional
 from math import sqrt, log
+import logging
 
 import torch
 from torch.nn.functional import one_hot
-from swiftmhc.modules.position_encoding import RelativePositionEncoder as SwiftMHCRelativePositionEncoder
 from position_encoding.relative import get_relative_position_encoding_matrix
 from position_encoding.absolute import get_absolute_position_encoding
 
 
-class TransformerEncoderLayer(torch.nn.Module):
-    def __init__(self,
-                 depth: int,
-                 n_head: int,
-                 dropout: Optional[float] = 0.1):
+_log = logging.getLogger(__name__)
 
-        super(TransformerEncoderLayer, self).__init__()
 
-        self.n_head = n_head
-        self.dropout = torch.nn.Dropout(dropout)
+class RelativePositionEncoder(torch.nn.Module):
+    """ 
+    Gives the input sequence a relative positional encoding and performs multi-headed attention.
+    """
 
-        self.linear_q = torch.nn.Linear(depth, depth * self.n_head, bias=False)
-        self.linear_k = torch.nn.Linear(depth, depth * self.n_head, bias=False)
-        self.linear_v = torch.nn.Linear(depth, depth * self.n_head, bias=False)
-
-        self.linear_o = torch.nn.Linear(self.n_head * depth, depth, bias=False)
-
-        self.norm_att = torch.nn.LayerNorm(depth)
-
-        self.ff_intermediary_depth = 128
-
-        self.mlp_ff = torch.nn.Sequential(
-            torch.nn.Linear(depth, self.ff_intermediary_depth),
-            torch.nn.ReLU(),
-            torch.nn.Linear(self.ff_intermediary_depth, depth),
-        )
-
-        self.norm_ff = torch.nn.LayerNorm(depth)
-
-    def self_attention(
+    def __init__(
         self,
-        seq: torch.Tensor,
-    ) -> torch.Tensor:
+        b_classification: bool,
+        no_heads: Optional[int] = 2,
+        length: Optional[int] = 9,
+        c_s: Optional[int] = 32,
+        dropout_rate: Optional[float] = 0.1,
+        c_transition: Optional[int] = 128,
+        c_hidden: Optional[int] = 16,
+    ):
+        """
+        Args:
+            b_classification:   classification(True) or regression(False)
+            no_heads:           number of attention heads
+            length(k):          determines the number of distance bins: [-k, -k + 1, ..., 0, ..., k - 1, k]
+            c_s:                the depth of the input tensor, at shape -1
+            dropout_rate:       for the dropouts before normalisation
+            c_transition:       transition depth in feed forward block
+            c_hidden:           the depth of the hidden tensors: attention query(q), keys(k), values(v)
+        """
 
-        batch_size, seq_len, d = seq.shape
+        super(RelativePositionEncoder, self).__init__()
 
-        # [batch_size, n_head, seq_len, d]
-        q = self.linear_q(seq).reshape(batch_size, seq_len, self.n_head, d).transpose(1, 2)
-        k = self.linear_k(seq).reshape(batch_size, seq_len, self.n_head, d).transpose(1, 2)
-        v = self.linear_v(seq).reshape(batch_size, seq_len, self.n_head, d).transpose(1, 2)
+        # constants
+        self.no_heads = no_heads
+        self.relpos_k = length
+        self.no_bins = 2 * self.relpos_k + 1
+        self.c_s = c_s
+        self.c_hidden = c_hidden
+        self.w_L = sqrt(1.0 / 2)  # because we have two terms
+        self.dropout_rate = dropout_rate
+        self.c_transition = c_transition
 
-        # [batch_size, n_head, seq_len, seq_len]
-        a = torch.matmul(q, k.transpose(2, 3)) / sqrt(d)
-        a = torch.softmax(a, dim=3)
+        # scaled dot multi-headed attention: queries, keys, values
+        self.linear_q = torch.nn.Linear(self.c_s, self.c_hidden * self.no_heads, bias=False)
+        self.linear_k = torch.nn.Linear(self.c_s, self.c_hidden * self.no_heads, bias=False)
+        self.linear_v = torch.nn.Linear(self.c_s, self.c_hidden * self.no_heads, bias=False)
 
-        # [batch_size, n_head, seq_len, d]
-        heads = torch.matmul(a, v)
+        # generates the b term in the attention weight
+        self.linear_b = torch.nn.Linear(self.no_bins, self.no_heads, bias=False)
 
-        # [batch_size, seq_len, d]
-        o = self.linear_o(heads.transpose(1, 2).contiguous().reshape(batch_size, seq_len, d * self.n_head))
+        # generates the output of the multi-headed attention
+        self.linear_output = torch.nn.Linear((self.no_bins + self.c_hidden) * self.no_heads, self.c_s)
 
-        return o
-
-    def feed_forward(self, seq: torch.Tensor) -> torch.Tensor:
-
-        o = self.mlp_ff(seq)
-
-        return o
-
-    def forward(self,
-                seq: torch.Tensor,
-    ) -> torch.Tensor:
-
-        x = seq
-
-        x = self.dropout(x)
-
-        y = self.self_attention(x)
-
-        y = self.dropout(y)
-        x = self.norm_att(x + y)
-
-        y = self.feed_forward(x)
-
-        y = self.dropout(y)
-        x = self.norm_ff(x + y)
-
-        return x
-
-
-class RelativePositionEncodingModel(torch.nn.Module):
-
-    def __init__(self, classification: bool):
-
-        super(RelativePositionEncodingModel, self).__init__()
-
-        o = 1
-        if classification:
-            o = 2
-
-        self.encoder1 = SwiftMHCRelativePositionEncoder(2, 16, 32)
-
-        self.peptide_norm = torch.nn.Sequential(
-            torch.nn.Dropout(p=0.1),
-            torch.nn.LayerNorm(32),
+        # to be used after multi-headed attention
+        self.norm1 = torch.nn.Sequential(
+            torch.nn.Dropout(self.dropout_rate),
+            torch.nn.LayerNorm(self.c_s),
         )
 
-        c_transition = 128
-        self.affinity_module = torch.nn.Sequential(
-            torch.nn.Linear(32, c_transition),
+        # to be used after multi-headed attention norm
+        self.feed_forward = torch.nn.Sequential(
+            torch.nn.Linear(self.c_s, self.c_transition),
             torch.nn.ReLU(),
-            torch.nn.Linear(c_transition, o),
+            torch.nn.Linear(self.c_transition, self.c_s),
         )
 
-    def forward(self, seq_embd: torch.Tensor) -> torch.Tensor:
+        # to be used after feed-forward
+        self.norm2 = torch.nn.Sequential(
+            torch.nn.Dropout(self.dropout_rate),
+            torch.nn.LayerNorm(self.c_s),
+        )
 
-        # [*, N, D]
-        x = seq_embd
+        # module's output dimension
+        self.out_dim = 1
+        if b_classification:
+            self.out_dim = 2
 
-        # [*, N]
-        mask = torch.ones(x.shape[:-1], dtype=torch.bool)
+        self.output = torch.nn.Linear(self.c_s, self.out_dim)
 
-        # [*, N, D]
-        y, a = self.encoder1(x, mask)
+    def compute_output(self, s: torch.Tensor) -> torch.Tensor:
 
-        # [*, N, D]
-        x = self.peptide_norm(x + y)
+        # [*, N_res, out_dim]
+        score = self.output(s)
 
-        # [*, N, o]
-        p = self.affinity_module(x)
+        # [*, out_dim]
+        ba = score.sum(dim=-2)
 
-        # [*, o]
-        return p.sum(dim=-2)
+        return ba
+
+    def forward(self, s: torch.Tensor) -> torch.Tensor:
+        """
+        Encodes a sequence, by means of self attention and feed forward MLP
+
+        Args:
+            s:      [*, 9, c_s]
+
+        Returns:
+            [*, out_dim]
+        """
+
+        s = self.norm1(s + self.attention(s))
+
+        s = self.norm2(s + self.feed_forward(s))
+
+        return self.compute_output(s)
+
+    def attention(self, s: torch.Tensor) -> torch.Tensor:
+        """
+        Performs multi-headed attention, but also takes relative positions into account.
+
+        Args:
+            s:      [*, N_res, c_s]
+
+        Returns:
+            updated s:  [*, N_res, c_s]
+        """
+
+        batch_size, maxlen, depth = s.shape
+
+        # [*, N_res, N_res, no_bins]
+        z = get_relative_position_encoding_matrix(maxlen, self.no_bins).to(device=s.device, dtype=s.dtype)
+        z = z[None, ...]
+
+        # [*, H, N_res, N_res]
+        b = self.linear_b(z).transpose(-2, -1).transpose(-3, -2)
+
+        # [*, H, N_res, c_hidden]
+        q = self.linear_q(s).reshape(batch_size, maxlen, self.c_hidden, self.no_heads).transpose(-2, -1).transpose(-3, -2)
+        k = self.linear_k(s).reshape(batch_size, maxlen, self.c_hidden, self.no_heads).transpose(-2, -1).transpose(-3, -2)
+        v = self.linear_v(s).reshape(batch_size, maxlen, self.c_hidden, self.no_heads).transpose(-2, -1).transpose(-3, -2)
+
+        # [*, H, N_res, N_res]
+        a = torch.nn.functional.softmax(
+            self.w_L * (torch.matmul(q, k.transpose(-2, -1)) / sqrt(self.c_hidden) + b),
+            dim=-1,
+        )
+
+        # [*, H, N_res, no_bins]
+        o_pair = (a.unsqueeze(-1) * z.unsqueeze(-4)).sum(-2)
+
+        # [*, H, N_res, c_hidden]
+        o = (a.unsqueeze(-1) * v.unsqueeze(-3)).sum(-2)
+
+        # [*, N_res, c_s]
+        embd = self.linear_output(
+            torch.cat(
+                (
+                    o_pair.transpose(-3, -2).reshape(batch_size, maxlen, self.no_heads * self.no_bins),
+                    o.transpose(-3, -2).reshape(batch_size, maxlen, self.no_heads * self.c_hidden),
+                ),
+                dim=-1
+            )
+        )
+
+        return embd
 
 
 class AbsolutePositionEncodingModel(torch.nn.Module):
